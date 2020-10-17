03d69e8bda7376eda8da7a95808c4644e611138a
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-10-17 23:43:08 +0200

    init commit

---
+++ routes/nav.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { auth } = require("./verifyUser");
+
+router.get("/", (req, res) => res.render("pages/index"));
+router.get("/signup", (req, res) => res.render("pages/signup"));
+router.get("/login", (req, res) => res.render("pages/login"));
+router.get("/dashboard", auth, (req, res) =>
+  res.render("pages/dashboard", { user: req.user })
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const cookieParser = require("cookie-parser");
+const navRoutes = require("./routes/nav");
+const userRoutes = require("./routes/users");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", navRoutes);
+app.use("/", userRoutes);
+
+app.listen(port, () => console.log(`Server up and running on port: ${port}`));
